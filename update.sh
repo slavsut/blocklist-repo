e167d3404d042412ef05f970726029fd871e68c8
slavsut <146323087+slavsut@users.noreply.github.com>
2026-04-06 18:54:10 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,4 +3,12 @@
 URL="https://iplists.firehol.org/files/firehol_level1.netset"
 OUT="blocklist.netset"
 
-curl -s $URL | grep -v '^#' | sed '/^$/d' > $OUT
+echo "[+] Downloading..."
+
+curl -A "Mozilla/5.0" -sf $URL \
+| grep -v '^#' \
+| sed '/^$/d' \
+> $OUT
+
+echo "[+] Lines:"
+wc -l $OUT

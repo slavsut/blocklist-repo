29c9aad3952992f83a761f12feb2e6862a5e2dc8
slavsut <146323087+slavsut@users.noreply.github.com>
2026-04-06 18:50:56 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,9 +3,4 @@
 URL="https://iplists.firehol.org/files/firehol_level1.netset"
 OUT="blocklist.netset"
 
-echo "[+] Downloading blocklist..."
-
 curl -s $URL | grep -v '^#' | sed '/^$/d' > $OUT
-
-echo "[+] Done. Lines:"
-wc -l $OUT
